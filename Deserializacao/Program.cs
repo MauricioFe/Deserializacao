0fd9c2ac1fe56735e2528811346baefd2a96544a
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-05 12:17:27 -0300

    Adionando comentários para melhor compreensão

--- Deserializacao/Program.cs
+++ Deserializacao/Program.cs
@@ -13,14 +13,17 @@ namespace Deserializacao
     {
         static void Main(string[] args)
         {
+            //alterar caminho do arquivo .Dat
             FileStream fs = new FileStream(@"C:\Users\mauri\Desktop\Treinamento\ProvasAprovadas\Sessao4SC\Projeto teste\SUGESTÃO SESSÃO 4 SC\DATA FILES\mapeamento.dat", FileMode.Open);
             // Cria um objeto BinaryFormatter para realizar a dessarialização
             BinaryFormatter bf = new BinaryFormatter();
+            //É preciso adicionar uma referência da dll BibliotecaMapa para que a desserialização funcione
             Mapa mapa =(Mapa)bf.Deserialize(fs);
 
             // fecha o arquivo
             fs.Close();
             // exibe o objeto desserializada
+            //Aqui temos um exeplos para mostrar alguns dados que chegam do arquivo. Pode ser alterado para a exibição de novos dados
             foreach (var item in mapa.Pontos)
             {
                 Console.WriteLine(item.X);
@@ -28,7 +31,6 @@ namespace Deserializacao
                 Console.WriteLine(item.Y);
             }
 
-            Console.WriteLine(mapa.Ligacoes[5,3]);
             Console.ReadKey();
         }
     }
